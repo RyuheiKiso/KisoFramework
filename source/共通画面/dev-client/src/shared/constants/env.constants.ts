74b27310f3eddc75874c3903f3b7a2d359853038
Ryuhei Kiso <uw2374@icloud.com>
2025-05-25 23:35:02 +0900

    feat: 環境設定ファイルにコメントを追加
    - env.ts: バックエンドAPIサーバのIPアドレスとポート番号を取得するためのコメントを追加
    - env.constants.ts: バックエンドAPIサーバのIPアドレスとポート番号に関するコメントを定数として明確化

--- source/共通画面/dev-client/src/shared/constants/env.constants.ts
+++ source/共通画面/dev-client/src/shared/constants/env.constants.ts
@@ -2,8 +2,8 @@
 
 import { BACKEND_IP, BACKEND_PORT } from '../config/env';
 
-// バックエンドAPIサーバのIPアドレス
+// バックエンドAPIサーバのIPアドレス定数
 export const API_SERVER_IP = BACKEND_IP;
 
-// バックエンドAPIサーバのポート番号
+// バックエンドAPIサーバのポート番号定数
 export const API_SERVER_PORT = BACKEND_PORT;
